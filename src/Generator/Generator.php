edebce25c88b37ba2caa674529554b6d14a33d27
aazsamir <sutikkukun@gmail.com>
2025-01-19 00:14:28 +0100

    move ref resolving to new class

--- src/Generator/Generator.php
+++ src/Generator/Generator.php
@@ -4,11 +4,6 @@ declare(strict_types=1);
 
 namespace Aazsamir\Libphpsky\Generator;
 
-use Aazsamir\Libphpsky\Generator\Lexicon\Def\Def;
-use Aazsamir\Libphpsky\Generator\Lexicon\Def\DefContainer;
-use Aazsamir\Libphpsky\Generator\Lexicon\Def\RefDef;
-use Aazsamir\Libphpsky\Generator\Lexicon\Def\UnionDef;
-use Aazsamir\Libphpsky\Generator\Lexicon\Lexicon;
 use Aazsamir\Libphpsky\Generator\Lexicon\Lexicons;
 use Aazsamir\Libphpsky\Generator\Loader\FileLexiconProvider;
 use Aazsamir\Libphpsky\Generator\Loader\Loader;
@@ -25,7 +20,8 @@ class Generator
             lexiconProvider: new FileLexiconProvider($path),
         );
         $lexicons = $loader->load();
-        $this->resolveRefs($lexicons);
+        $refResolver = new RefResolver();
+        $refResolver->resolveRefs($lexicons);
 
         $config = new MakeConfig(
             path: __DIR__ . '/../Model',
@@ -39,74 +35,5 @@ class Generator
         $maker->make($lexicons);
 
         return $lexicons;
-    }
-
-    private function resolveRefs(Lexicons $lexicons): Lexicons
-    {
-        foreach ($lexicons->toArray() as $lexicon) {
-            $this->resolveLexicon($lexicons, $lexicon);
-        }
-
-        return $lexicons;
-    }
-
-    private function resolveLexicon(Lexicons $lexicons, Lexicon $lexicon): void
-    {
-        foreach ($lexicon->defs()->toArray() as $def) {
-            $this->resolveDef($lexicons, $lexicon, $def);
-        }
-    }
-
-    private function resolveDef(Lexicons $lexicons, Lexicon $lexicon, Def $def): void
-    {
-        switch (true) {
-            case $def instanceof DefContainer:
-                foreach ($def->defs()->toArray() as $def) {
-                    $this->resolveDef($lexicons, $lexicon, $def);
-                }
-
-                return;
-
-            case $def instanceof UnionDef:
-                $refs = [];
-
-                foreach ($def->refs() as $ref) {
-                    $found = $lexicon->findDefByRef($ref, $lexicon->id());
-
-                    if ($found === null) {
-                        $found = $lexicons->findDefByRef($ref);
-                    }
-
-                    if ($found !== null) {
-                        $refs[] = $found;
-                    }
-                }
-
-                $def->setResolvedRefs($refs);
-
-                return;
-        }
-
-        if (!($def instanceof RefDef)) {
-            return;
-        }
-
-        $refid = $def->ref();
-
-        if (!str_contains($refid, '#')) {
-            $refid .= '#main';
-        }
-
-        $referenced = $lexicon->findDefByRef($refid, $lexicon->id());
-
-        if ($referenced === null) {
-            $referenced = $lexicons->findDefByRef($refid);
-        }
-
-        if ($referenced === null) {
-            throw new \RuntimeException("Cannot find referenced def: {$refid}");
-        }
-
-        $def->setResolvedDef($referenced);
     }
 }
