cafe96ad78fd2cc4234371b557ac6ce5fff82a11
aazsamir <sutikkukun@gmail.com>
2025-01-19 14:53:15 +0100

    FileSaveClass tests

---
+++ tests/Unit/Generator/Maker/FileSaveClassTest.php
@@ -0,0 +1,61 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Generator\Maker;
+
+use Aazsamir\Libphpsky\Generator\Maker\FileSaveClass;
+use Aazsamir\Libphpsky\Generator\Maker\MakeConfig;
+use Nette\PhpGenerator\ClassType;
+use Nette\PhpGenerator\PhpNamespace;
+use Tests\Unit\TestCase;
+
+class FileSaveClassTest extends TestCase
+{
+    private FileSaveClass $fileSaveClass;
+    private MakeConfig $makeConfig;
+    private string $path = __DIR__ . '/artifacts';
+
+    protected function setUp(): void
+    {
+        $this->deleteDir();
+        $this->makeConfig = new MakeConfig(
+            path: $this->path,
+            namespace: 'Tests\Artifacts',
+        );
+        $this->fileSaveClass = new FileSaveClass($this->makeConfig);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->deleteDir();
+    }
+
+    private function deleteDir(): void
+    {
+        $dir = $this->path;
+        $files = array_diff(scandir($dir) ?: [], ['.', '..']);
+
+        foreach ($files as $file) {
+            unlink("$dir/$file");
+        }
+
+        rmdir($dir);
+    }
+
+    public function testSave(): void
+    {
+        $class = new ClassType('TestClass');
+        $namespace = new PhpNamespace($this->makeConfig->namespace);
+        
+        $this->fileSaveClass->save($class, $namespace);
+
+        $this->assertFileExists($this->path . '/TestClass.php');
+
+        $content = file_get_contents($this->path . '/TestClass.php');
+
+        $this->assertStringContainsString('declare(strict_types=1);', $content);
+        $this->assertStringContainsString('namespace Tests\Artifacts;', $content);
+        $this->assertStringContainsString('class TestClass', $content);
+    }
+}
