9d868299eb742284a655aacfdd7a636278b29ba7
aazsamir <sutikkukun@gmail.com>
2025-10-14 21:19:21 +0200

    test that generator result match files in repo

---
+++ tests/Integration/GeneratorTest.php
@@ -0,0 +1,55 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Integration;
+
+use Aazsamir\Libphpsky\Generator\Generator;
+use Aazsamir\Libphpsky\Generator\Maker\MakeConfig;
+use Aazsamir\Libphpsky\Generator\Maker\MakeConfigEntry;
+
+class GeneratorTest extends TestCase
+{
+    public function testGeneratorResultMatchRepo(): void
+    {
+        $generator = Generator::default();
+        $originDir = __DIR__ . '/../../src/Model';
+        $tempDir = sys_get_temp_dir() . '/' . \uniqid() . '/src/Model';
+
+        $configEntry = new MakeConfigEntry(
+            lexiconsPath: __DIR__ . '/../../lexicons',
+            path: $tempDir,
+            namespace: 'Aazsamir\Libphpsky\Model',
+            metaClient: true,
+            generate: true,
+        );
+        $config = new MakeConfig([$configEntry]);
+        [$repoHash, $repoCount] = $this->calculateDirectoryHash($originDir);
+
+        $generator->generate($config);
+
+        [$tempHash, $tempCount] = $this->calculateDirectoryHash($tempDir);
+
+        $this->assertEquals($repoCount, $tempCount, 'Generated Model files count do not match repository files. Remove "src/Model" and run "php generate.php".');
+        $this->assertEquals($repoHash, $tempHash, 'Generated Model files do not match repository files. Run "php generate.php".');
+    }
+
+    /**
+     * @return array{string, int}
+     */
+    private function calculateDirectoryHash(string $directory): array
+    {
+        // iterate over files in directory, recursively
+        $files = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($directory));
+        $hashes = [];
+        foreach ($files as $file) {
+            /** @var \SplFileInfo $file */
+            if ($file->isFile() && $file->getExtension() === 'php') {
+                $hashes[] = \md5_file($file->getRealPath());
+            }
+        }
+        sort($hashes);
+
+        return [md5(implode('', $hashes)), count($hashes)];
+    }
+}
